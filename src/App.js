bb7ed1108f630e7b86a1d03fd3c5882196375778
Mohamed-code-ship <mohaabdul568@gmail.com>
2023-09-21 12:38:27 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+import Best from './components/best/Best';
+import Feature from './components/feature/Featured';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+       <Navbar/>
+       <Hero/>
+       <Best/>
+       <Feature/>
+       <Footer/>
+    </>
   );
 }
+  
+  
 
 export default App;
